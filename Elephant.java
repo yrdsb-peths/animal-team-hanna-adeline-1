6c61f6ea186b2f165e4b173143842924236fbadc
Adeline Lai <adelinelai08@gmail.com>
2025-12-02 13:02:13 -0500

    Increased Elephant size and made its trunk move

--- Elephant.java
+++ Elephant.java
@@ -9,19 +9,68 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("Elephant Sound.mp3");
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    GreenfootImage [] idleRight = new GreenfootImage[8];
+    /**
+     * Constructor - The code that gets run one time when object is created
+     */
+    // Direction the elephant is facing
+    String facing = "right";
+    
+    public Elephant()
+    {
+        for(int i = 0; i<idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i<idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        setImage(idleRight[0]);
+    }
+    
+    /*
+     * Animate the elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-4);
+            move(-2);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(4);
+            move(2);
+            facing = "right";
         }
         
         //Remove apple if elephant eats it
         eat();
+        
+        // Animate the elephant
+        animateElephant();
     }
     
     /**
