c5aefaf45f2fbc547b72e3dfddd5eae3fc8066c8
Adeline Lai <adelinelai08@gmail.com>
2025-12-02 12:15:51 -0500

    Revert "Added sprites for Elephant"
    This reverts commit 30d4743cf69970be581362c45dcb4872e1c5cae3.

--- Elephant.java
+++ Elephant.java
@@ -1,93 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The Elephant, our hero.
+ * Write a description of class Elephant here.
  * 
- * @author Adeline Lai
- * @version November 2025
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class Elephant extends Actor
 {
-    GreenfootSound elephantSound = new GreenfootSound("elephantsound.mp3");
-    GreenfootImage[] idleRight = new GreenfootImage[8];
-    GreenfootImage[] idleLeft = new GreenfootImage[8];
-    
-    // Direction the elephant is facing
-    String facing = "right";
-    SimpleTimer animationTimer = new SimpleTimer();
-    
-    
-    /**
-     * Constructor - The code that gets run one time when object is created
-     */
-    public Elephant()
-    {
-        for(int i = 0; i < idleRight.length; i++)
-        {
-            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
-            idleRight[i].scale(100,100);
-        }
-        
-        for(int i = 0; i < idleLeft.length; i++)
-        {
-            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
-            idleLeft[i].mirrorHorizontally();
-            idleLeft[i].scale(100,100);
-        }        
-        
-        animationTimer.mark();
-        
-        // Initial elephant image
-        setImage(idleRight[0]);
-    }
-    
-    /**
-     * Animate the elephant
-     */
-    int imageIndex = 0;
-    public void animateElephant()
-    {
-        if(animationTimer.millisElapsed() < 150)
-        {
-            return;
-        }
-        animationTimer.mark();
-        
-        if(facing.equals("right"))
-        {
-            setImage(idleRight[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleRight.length;
-        }
-        else
-        {
-            setImage(idleLeft[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleLeft.length;
-        }
-        
-    }
-    
+    GreenfootSound elephantSound = new GreenfootSound("Elephant Sound.mp3");
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-3);
-            facing = "left";
+            move(-4);
         }
-        if(Greenfoot.isKeyDown("right"))
+        else if(Greenfoot.isKeyDown("right"))
         {
-            move(3);
-            facing = "right";
+            move(4);
         }
         
         //Remove apple if elephant eats it
         eat();
-        
-        // Animate the elephant
-        animateElephant();
     }
     
     /**
-     * Eat the apple and spawn new apple if an apple is eaten 
+     * Eat the apple and spawn new apple if an apple is eaten.
      */
     public void eat()
     {
