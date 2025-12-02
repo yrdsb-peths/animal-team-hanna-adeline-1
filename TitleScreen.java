917b6813d0bdab0f4614b1dcce603f4d4d07192f
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-12-02 09:44:52 -0500

    Create TitleScreen.java

---
+++ TitleScreen.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen
+ * 
+ * @author Hanna, Adeline 
+ * @version December 2025
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 65);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+}
